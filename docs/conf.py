241278dca9e122eeb5f46ec6c2dd62f476b10094
jean-philippe serafin <serafinjp@gmail.com>
2011-09-28 15:56:29 +0200

    changed screenshot

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ release = '0.1a'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
